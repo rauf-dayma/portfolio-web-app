e0c931ceb2bba5342757eb082e660b08d3fa4d6b
rauf-dayma <daymarauf@gmail.com>
2024-09-26 15:28:36 +0530

    innitial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const modeToggle = document.getElementById('mode-toggle');
+
+// modeToggle.addEventListener('click', function() {
+//   // Toggle dark mode class on body
+//   document.body.classList.toggle('dark-mode');
+
+//   // Check if dark mode is active
+//   const isDarkMode = document.body.classList.contains('dark-mode');
+  
+//   // Update icon based on dark mode state
+//   if (isDarkMode) {
+//     modeToggle.style.backgroundImage = 'url("night-mode.png")';
+//     // If dark mode is active, update background and text color
+//     document.body.style.backgroundColor = '#222';
+//     document.body.style.color = '#fff';
+//   } else {
+//     modeToggle.style.backgroundImage = 'url("day-mode.png")';
+//     // If dark mode is inactive, reset background and text color
+//     document.body.style.backgroundColor = '#f8f8f8';
+//     document.body.style.color = '#333';
+//   }
+// });
